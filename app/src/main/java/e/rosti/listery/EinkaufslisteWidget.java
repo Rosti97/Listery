3c2d9383e8fc53808e2dbf5308585acea477fd8b
Rosti <das.rosti@gmail.com>
2018-09-09 14:31:53 +0200

    [CHANGED] EinkaufslisteWidget

--- app/src/main/java/e/rosti/listery/EinkaufslisteWidget.java
+++ app/src/main/java/e/rosti/listery/EinkaufslisteWidget.java
@@ -2,13 +2,11 @@ package e.rosti.listery;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.arch.lifecycle.LiveData;
+import android.content.ComponentName;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.RemoteViews;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,15 +17,11 @@ public class EinkaufslisteWidget extends AppWidgetProvider {
     static void updateAppWidget(final Context context, final AppWidgetManager appWidgetManager,
                                 final int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.einkaufsliste_widget);
 
         List<Item> allItems = null;
 
-
-        /**TODO DATENBANK**/
-        //Einkaufsliste holen
 
         new AsyncTask<Void, Void, List<Item>>(){
             @Override
@@ -40,6 +34,8 @@ public class EinkaufslisteWidget extends AppWidgetProvider {
 
                     views.setTextViewText(R.id.appwidget_text, anzeige);
                     views.setTextViewText(R.id.appwidget_text_anzahl, produkte);
+                    appWidgetManager.updateAppWidget(new ComponentName(context, EinkaufslisteWidget.class), views);
+
                 }
             }
 
@@ -51,13 +47,7 @@ public class EinkaufslisteWidget extends AppWidgetProvider {
                 return list;
             }
         }.execute();
-        //Einkaufsliste nach checked suchen, Methode kann so bleiben
-        /*ArrayList<Einkaufsitem> checkedList = new ArrayList<>();
-        for (Einkaufsitem item: list) {
-            if(item.isChecked()) {
-                checkedList.add(item);
-            }
-        }*/
+
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
